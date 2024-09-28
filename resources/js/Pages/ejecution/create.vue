fcc6b0bff6fab3a71d1a1df0f1c72b5f6ca6854c
oscarlopezv <oscarlopez_v@hotmail.com>
2024-09-28 13:02:33 -0500

    Update create.vue

--- resources/js/Pages/ejecution/create.vue
+++ resources/js/Pages/ejecution/create.vue
@@ -54,13 +54,12 @@ const obtenerArchivos = (e) =>
 
                         <div class="mb-5">
                             <label for="" class="block mb-2 text-sm font-medium text-gray-900 ">Descripción*</label>
-                            <textarea id="" name="" v-model="form.description" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base"></textarea>
+                            <textarea id="" name="" v-model="form.description" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base" required></textarea>
                         </div>
 
                         <svg @click="agregarCampo()" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-9 text-blue-800 ">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                         </svg>
-                        
                         
                         <div class="grid grid-cols-3 gap-6">
                             <div class="" v-for="(item,index) in inputs" :key="index" >
